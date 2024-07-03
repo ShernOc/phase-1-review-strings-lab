4897611bd1f22a8a84bf450a40540f558eaedabf
Sherlyne <sherlynea1994@hotmail.com>
2024-07-03 13:02:27 +0300

    Modified String lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code in this file!
+const currentUser = 'Sherlyne';
+console.log(currentUser); 
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+console.log (welcomeMessage); 
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage); 
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+console.log(excitedWelcomeMessage); 
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,2)}!`;  
+console.log (shortGreeting); 
+
+
+
+
+
+
+
